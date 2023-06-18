004d6d1c166e961933622c324ac9fbe55af13234
Drpto <dr.juniornoob@gmail.com>
2023-06-17 20:50:00 -0700

    violet: DroidX
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
-#
+\#
 # Copyright (C) 2018-2020 The LineageOS Project
-# Copyright (C) 2020 The PixelExperience Project
+# Copyright (C) 2020 The DroidXUI Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #

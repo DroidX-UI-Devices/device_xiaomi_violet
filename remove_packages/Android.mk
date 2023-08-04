2ee1b641a7c9bd99e0f71feec9cd9696ed7d0dc9
Drpto <dr.juniornoob@gmail.com>
2023-08-03 23:24:43 -0700

    violet: Nuck some apps from remove packages
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,8 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := CalculatorGooglePrebuilt CalendarGooglePrebuilt PrebuiltDeskClockGoogle Maps Drive PrebuiltGmail YouTube Videos 
-LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2 GoogleFeedback Aperture talkback
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube Videos
+LOCAL_OVERRIDES_PACKAGES += TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2 GoogleFeedback Aperture talkback
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null

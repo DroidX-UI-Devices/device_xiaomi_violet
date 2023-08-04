e3fdacd2339f906a93474340602766f947658e1b
Drpto <dr.juniornoob@gmail.com>
2023-08-04 00:45:59 -0700

    violet: Nuck proton clang
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,6 @@ git clone https://github.com/SuperiorOS-Devices/vendor_xiaomi_violet.git -b thir
 git clone https://github.com/SuperiorOS-Devices/kernel_xiaomi_violet.git -b thirteen kernel/xiaomi/violet --depth=1
 rm -rf prebuilts/clang/host/linux-x86/clang-r487747
 git clone https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r487747.git -b master prebuilts/clang/host/linux-x86/clang-r487747 --depth=1
-git clone https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
 git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi-firmware.git -b thirteen vendor/xiaomi-firmware --depth=1
 rm -rf packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-20.0 packages/resources/devicesettings --depth=1

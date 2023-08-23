6d83ad2b9fc69f68710d08bf77cfed59e18ed76a
Drpto <dr.juniornoob@gmail.com>
2023-08-23 10:40:57 -0700

    violet: Track clang 17.0.3 from https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r498229.git
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,8 @@
 git clone https://gitlab.com/Drpto/android_vendor_MiuiCamera.git -b thirteen vendor/MiuiCamera
 git clone https://github.com/SuperiorOS-Devices/vendor_xiaomi_violet.git -b thirteen vendor/xiaomi/violet --depth=1
 git clone https://github.com/SuperiorOS-Devices/kernel_xiaomi_violet.git -b thirteen kernel/xiaomi/violet --depth=1
-rm -rf prebuilts/clang/host/linux-x86/clang-r487747
-git clone https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r487747.git -b master prebuilts/clang/host/linux-x86/clang-r487747 --depth=1
+rm -rf prebuilts/clang/host/linux-x86/clang-r498229
+git clone https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r498229.git -b master prebuilts/clang/host/linux-x86/clang-r498229 --depth=1
 git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi-firmware.git -b thirteen vendor/xiaomi-firmware --depth=1
 rm -rf packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-20.0 packages/resources/devicesettings --depth=1

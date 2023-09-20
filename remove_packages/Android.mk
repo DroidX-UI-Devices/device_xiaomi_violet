13672f3dc88f871158f66abb2e86c777c51abad3
Drpto <dr.juniornoob@gmail.com>
2023-09-20 21:22:55 +0530

    violet: Add more apps to nucked while builds
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube Videos
+LOCAL_OVERRIDES_PACKAGES := Maps Drive PrebuiltGmail YouTube Videos RecorderPrebuilt recorder documentsui
 LOCAL_OVERRIDES_PACKAGES += TipsPrebuilt arcore SafetyHubPrebuilt AndroidAutoStubPrebuilt Camera2 GoogleFeedback Aperture talkback
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
